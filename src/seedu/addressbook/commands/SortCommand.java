28a5c90183c26181274d2039079004b88dfc08d7
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2019-02-09 19:53:44 +0800

    Add command `sort` that does nothing

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,18 @@
+package seedu.addressbook.commands;
+
+/**
+ * Sorts the address book.
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+    public static final String MESSAGE_USAGE = "Sorts address book permanently in alphabetical order of names.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_SUCCESS = "Address book has been sorted!";
+
+    @Override
+    public CommandResult execute() {
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
